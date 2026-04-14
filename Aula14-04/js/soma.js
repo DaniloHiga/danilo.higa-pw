f5c0970d8040b5cc3ea46bcb09074ad0802f1076
Danilo Higa <danikehiga@gmail.com>
2026-04-14 08:14:06 -0300

    1° aula de PW II

---
+++ Aula14-04/js/soma.js
@@ -0,0 +1,6 @@
+var num1 = parseFloat(prompt("Digite o primeiro numero: "));
+var num2 = parseFloat(prompt("Digite o segundo numero: "));
+
+var soma = num1+num2;
+
+console.log("A soma dos numeros é: " ,soma);
