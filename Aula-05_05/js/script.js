705f8c3e193fc4d599648447d250a4d6ec13ebfb
Danilo <danikehiga@gmail.com>
2026-05-05 08:01:51 -0300

    Aula de PW 05/05

---
+++ Aula-05_05/js/script.js
@@ -0,0 +1,28 @@
+//Estrutura de repetição While
+var x=0;
+var soma=0;
+while (x<=5){
+    soma=soma+x;
+    x++ 
+}
+console.log(soma);
+
+
+//Criar um array passando por todos os elementos
+var arr=['teste1', 'teste2', 'teste3', 'teste4'];
+var y=0;
+//Passando por todos os elementos
+while (y<=3){
+    console.log(arr[y]);
+    y++
+}
+
+
+//Passando por todas as palavras
+var palavra="Danilo";
+var i=0;
+//Passando por todos os elementos
+while (i<=5){
+    console.log(palavra[i]);
+    i++
+}
