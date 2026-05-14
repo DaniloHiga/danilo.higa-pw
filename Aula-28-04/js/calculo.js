798b3a80abc932541077bcc7e70e4d96bc5bee90
Danilo <danikehiga@gmail.com>
2026-05-13 23:06:43 -0300

    Aula 28/04 / Aula 12/05 CSS e JS

--- Aula-28-04/js/calculo.js
+++ Aula-28-04/js/calculo.js
@@ -26,3 +26,27 @@ function areatriangulo() {
 function areaquadrado() {
     resultado.innerHTML = Number(n1.value) * Number(n2.value)
 }
+function menosdez() {
+    resultado.innerHTML = Number(n1.value) - (Number(n1.value)*0.10)
+}
+function lucro() {
+    resultado.innerHTML = Number(n1.value) + (Number(n1.value)*0.15)
+}
+function desconto() {
+    resultado.innerHTML = Number(n1.value) - (Number(n1.value)*0.03)
+}
+function lucdesc() {
+    if (Number(n1.value)>1000) {
+        resultado.innerHTML = Number(n1.value) - (Number(n1.value)*0.07);
+    }
+    else if ((Number(n1.value)<200) && (Number(n1.value)<=500)) {
+        alert("Boa compra!");
+        resultado.innerHTML = Number(n1.value) + (Number(n1.value)*0.05);
+    }
+    else if (Number(n1.value)<=500) {
+        resultado.innerHTML = Number(n1.value) + (Number(n1.value)*0.05);
+    }
+    else {
+        resultado.innerHTML = Number(n1.value)
+    }
+}
