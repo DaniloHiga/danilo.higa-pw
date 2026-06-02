62ec62c926f1ddaa32df171d5f59ceb0e4a28bb1
Danilo <danikehiga@gmail.com>
2026-06-02 08:30:02 -0300

    Site da Loja

---
+++ Site_Loja/js/script.js
@@ -0,0 +1,29 @@
+function altQtd(qtdId, precold, totalId, valor) {
+    let qtd = document.getElementById(qtdId);
+    let quantidade = parseInt(qtd.innerText);
+
+    quantidade += valor;
+
+    if (quantidade<0) {
+        quantidade=0;
+    }
+
+    qtd.innerText = quantidade;
+
+let preço = parseFloat(document.getElementById(precold).innerText);
+
+let total = quantidade*preço;
+
+document.getElementById(totalId).innerText = "R$" + total.toFixed(2);
+
+function calcularSubtotal() {
+    let total1=parseFloat(document.getElementById("total1").innerText);
+    let total2=parseFloat(document.getElementById("total2").innerText);
+    let total3=parseFloat(document.getElementById("total3").innerText);
+
+    let Subtotal = total1 + total2 + total3;
+
+    document.getElementById("Subtotal").innerText = "Subtotal: R$" + Subtotal.toFixed(2);
+}
+calcularSubtotal();
+}   
